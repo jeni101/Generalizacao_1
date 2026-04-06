0b07abbced58351497d0b12a7a83fe82620f30c7
epilihP <philipegcustodio@gmail.com>
2026-04-05 21:57:13 -0300

    (simpson 1/3 add simpson13_graph.py in main)

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from simpson38.simpson38_graph import perform
+from simpson38.simpson13_graph import perform_13
 
 # Simpson 3/8
 # python main.py perform
@@ -8,3 +9,6 @@ funcao_simpson = "np.sin(x)**2"
 # Deve ser um múltiplo de 3
 numero = 999
 perform(funcao_simpson, 0, np.pi, numero)
+
+numero_13 = 1000
+perform_13(funcao_simpson, 0, np.pi, numero_13)
